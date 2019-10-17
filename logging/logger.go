3ae9757df2adef108beadc7c4bbfe5cb4c733951
nyo <nyo4rdace@gmail.com>
2019-10-17 21:20:21 +0800

    add rolling logger

--- logging/logger.go
+++ logging/logger.go
@@ -1,6 +1,12 @@
 package logging
 
-import "go.uber.org/zap"
+import (
+	"os"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"gopkg.in/natefinch/lumberjack.v2"
+)
 
 var logger *zap.Logger
 var slogger *zap.SugaredLogger
@@ -40,4 +46,37 @@ func L() *zap.Logger {
 // S return zap sugared logger
 func S() *zap.SugaredLogger {
 	return slogger
+}
+
+// NewRollingLogger will rotate log file
+func NewRollingLogger() *zap.Logger {
+	writeFile := zapcore.AddSync(&lumberjack.Logger{
+		Filename: "service.log",
+		MaxSize:  10, // megabytes
+		MaxAge:   28, // days
+	})
+	writeStdout := zapcore.AddSync(os.Stdout)
+
+	core := zapcore.NewCore(
+		zapcore.NewJSONEncoder(zapcore.EncoderConfig{
+			TimeKey:        "ts",
+			LevelKey:       "level",
+			NameKey:        "logger",
+			CallerKey:      "caller",
+			MessageKey:     "msg",
+			StacktraceKey:  "stacktrace",
+			LineEnding:     zapcore.DefaultLineEnding,
+			EncodeLevel:    zapcore.LowercaseLevelEncoder,
+			EncodeTime:     zapcore.ISO8601TimeEncoder,
+			EncodeDuration: zapcore.SecondsDurationEncoder,
+			EncodeCaller:   zapcore.ShortCallerEncoder,
+		}),
+		zapcore.NewMultiWriteSyncer(writeFile, writeStdout),
+		zap.NewAtomicLevel(),
+	)
+	return zap.New(
+		core,
+		zap.AddCaller(),
+		zap.AddStacktrace(zap.ErrorLevel),
+	)
 }
